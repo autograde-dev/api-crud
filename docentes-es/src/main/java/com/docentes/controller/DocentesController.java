9ecd646bf79ebd59074afefb9aa64fc18307ac6f
nc89 <nicolas.cardenas@unillanos.edu.co>
2024-10-22 20:15:11 -0500

    add swagger

--- docentes-es/src/main/java/com/docentes/controller/DocentesController.java
+++ docentes-es/src/main/java/com/docentes/controller/DocentesController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.docentes.entity.DocentesEntity;
 import com.docentes.service.DocentesService;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8081")
@@ -25,6 +26,7 @@ public class DocentesController {
     @Autowired
     private DocentesService docentesService;
 
+    @Operation(summary = "Listar todos los docentes")
     @GetMapping("/find-all")
     public ResponseEntity<?> getEstudiantes() {
         var docentes = docentesService.getDocentes();
@@ -34,33 +36,37 @@ public class DocentesController {
         return new ResponseEntity<>(docentes, HttpStatus.OK);
     }
 
+    @Operation(summary = "Listar docentes por pagina")
     @GetMapping("/findall-page/{page}/{size}")
-    public ResponseEntity<?> findPage(@PathVariable  int page, @PathVariable int size) {
-        var docentes= docentesService.findPage(page,size);
-        if(docentes == null){
+    public ResponseEntity<?> findPage(@PathVariable int page, @PathVariable int size) {
+        var docentes = docentesService.findPage(page, size);
+        if (docentes == null) {
             return new ResponseEntity<>("{}", HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(docentes, HttpStatus.OK);
     }
 
+    @Operation(summary = "Buscar docente por id")
     @GetMapping("find-by-id/{id}")
-    public ResponseEntity<?> getEstudiantesbyId(@PathVariable("id") Long id) {
+    public ResponseEntity<?> getDocentebyId(@PathVariable("id") Long id) {
         var docente = docentesService.getDocentesbyId(id);
-        if (docente == null || docente.getIdDocente()==null) {
+        if (docente == null || docente.getIdDocente() == null) {
             return new ResponseEntity<>("{}", HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(docente, HttpStatus.OK);
     }
 
+    @Operation(summary = "Buscar docente por documento")
     @GetMapping("find-by-documento/{documento}")
     public ResponseEntity<?> getEstudianteByDocumento(@PathVariable("documento") String documento) {
         var docente = docentesService.getDocentebyDocumento(documento);
-        if (docente == null || docente.getIdDocente()==null) {
+        if (docente == null || docente.getIdDocente() == null) {
             return new ResponseEntity<>("{}", HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(docente, HttpStatus.OK);
     }
 
+    @Operation(summary = "Crear docente")
     @PostMapping("insertar/")
     public ResponseEntity<MessageDto> insertarDocente(@RequestBody DocentesEntity docentesEntity) {
         boolean response = docentesService.save(docentesEntity);
@@ -70,6 +76,7 @@ public class DocentesController {
         return ResponseEntity.internalServerError().body(new MessageDto(HttpStatus.INTERNAL_SERVER_ERROR, "Error Guardando"));
     }
 
+    @Operation(summary = "Eliminar docente por id")
     @DeleteMapping("borrar/{id}")
     public ResponseEntity<MessageDto> borrarDocente(@PathVariable long id) {
         boolean response = docentesService.deleteById(id);
@@ -79,6 +86,7 @@ public class DocentesController {
         return ResponseEntity.internalServerError().body(new MessageDto(HttpStatus.INTERNAL_SERVER_ERROR, "Error Eliminando"));
     }
 
+    @Operation(summary = "Actualizar docente por id")
     @PutMapping("actualizar/{id}")
     public ResponseEntity<MessageDto> updateDocente(@RequestBody DocentesEntity docentesEntity) {
         boolean response = docentesService.update(docentesEntity);
